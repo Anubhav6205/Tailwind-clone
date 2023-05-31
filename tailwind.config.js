8bb8cc3e865509f731d75fd7b21057c14a6496e9
Anubhav Gupta <2005853@kiit.ac.in>
2023-06-01 00:09:28 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: ['./index.html'], // Add your HTML files here
+  // ...
+};
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'rgb-255-255-255': 'rgb(255, 255, 255)',
+        // Add more RGB color definitions as needed
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
